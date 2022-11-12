5e206681a83ae16afc2b5d455577cb0b45b35b51
Dancyangelo102 <chiomaubaezuonu102@gmail.com>
2022-11-12 06:12:11 +0100

    Added Navbar and Airbnb Components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import MetaPlace from './Components/Airbnb'
+import Airbnb from './Components/Airbnb'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Airbnb />
+    </div>
+  )
+}
+
+export default App
