9127d61ab4f44fc5da58d55db4dd6f82d45b5508
Dancyangelo102 <chiomaubaezuonu102@gmail.com>
2022-11-16 08:44:33 +0100

    Modidfied Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -24,10 +24,10 @@ const Navbar = () => {
           <Link to="/places-to-stay">
             <span className="link">Place to stay</span>
           </Link>
-          <a href="!#">
+          <a href="!# ">
             <span className="link">NFTs</span>
           </a>
-          <a href="!#">
+          <a href="!# ">
             <span className="link">Community</span>
           </a>
         </div>
